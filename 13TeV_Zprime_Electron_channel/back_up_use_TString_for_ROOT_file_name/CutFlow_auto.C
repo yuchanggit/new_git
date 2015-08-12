62706643bb20341ade8e38845484ac393f3173b4
Yu-hsiang <index0192@yahoo.com.tw>
2015-08-12 09:50:07 +0200

    [comment]

---
+++ 13TeV_Zprime_Electron_channel/back_up_use_TString_for_ROOT_file_name/CutFlow_auto.C
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <fstream>
+#include <TString.h>
+#include <TH1D.h>
+#include <TFile.h>
+#include <string>
+#include "TCanvas.h"
+#include "xAna_ele.C"
+#include "xAna_ele_noCuts.C"
+#include "xAna_ele_match_rate.C"
+#include "xAna_ele_compare_ChingWei.C"
+
+void CutFlow_auto(){
+
+const int N_mass_points = 13;
+
+/*
+  char* file_name[N_mass_points] = {
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-600_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-800_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1200_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1400_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1600_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1800_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-2000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-2500_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-3000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-3500_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-4000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-4500_13TeV-madgraph.root"
+			};
+*/
+
+TString file_name[N_mass_points] = {
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-600_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-800_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1200_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1400_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1600_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-1800_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-2000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-2500_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-3000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-3500_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-4000_13TeV-madgraph.root",
+"ZprimeZhbb/ZprimeToZhToZlephbb_narrow_M-4500_13TeV-madgraph.root"
+                        };
+
+TString path = "/Users/index0192/13_TeV_Task/";
+
+for(int i=0;i<N_mass_points;i++)
+{ file_name[i] = path + file_name[i];      }
+
+  int mass_point[N_mass_points]={600,800,1000,1200,1400,1600,1800,2000,2500,3000,3500,4000,4500};
+
+TCanvas *c1[N_mass_points];
+TCanvas *c2[N_mass_points];
+TCanvas *c3[N_mass_points];
+TCanvas *c4[N_mass_points];
+TCanvas *c5[N_mass_points];
+
+int dir_flag =0;// choose which Cut you want
+
+for(int j=0;j<10;j++){
+
+continue;
+
+dir_flag = j;
+
+if(j!=7 && j!=8 && j!=9 )continue;
+
+TString dir_name; 
+if (dir_flag ==0){ dir_name = "plot_NoCuts/";}
+if (dir_flag ==1){ dir_name = "plot_TurnOff3Cuts/";}
+if (dir_flag ==2){ dir_name = "plot_Cut_SD40to140_ElePt85_llPt200/"; }
+if (dir_flag ==3){ dir_name = "plot_Cut_SD50to140_ElePt85_llPt200/";}
+if (dir_flag ==4){ dir_name = "plot_Cut_SD60to140_ElePt100_llPt200/";}
+if (dir_flag ==5){ dir_name = "plot_Cut_SD60to140_ElePt115_llPt200/";}
+if (dir_flag ==6){ dir_name = "plot_Cut_SD60to140_ElePt85_llPt200/";}
+
+if (dir_flag ==7){ dir_name = "plot_Cut_SD93to143_ElePt115_llPt200/";}
+if (dir_flag ==8){ dir_name = "plot_Cut_SD95to145_ElePt115_llPt200/";}
+if (dir_flag ==9){ dir_name = "plot_Cut_SD91to141_ElePt115_llPt200/";}
+
+  double eff, eff_err;
+
+for (int i=0;i<13;i++){// 13 mass points
+
+    if (i!=2) continue;// M= 1000
+//  if(i!=0 && i!= 12){continue;}
+  cout<<"i: "<<i <<"  mass_point[i]: "<< mass_point[i]<<endl;
+
+  eff=-99; eff_err=-99;
+
+  TString cut_flow_name = Form("Zprime_Cut_Flow_M_%d",mass_point[i]);
+  TString ele_pT_name = Form("Zprime_ele_pT_M_%d",mass_point[i]);
+  TString ll_pT_name = Form("Zprime_ll_pT_M_%d",mass_point[i]);
+  TString SD_mass_name = Form("Zprime_SD_mass_M_%d",mass_point[i]);
+  TString Z_mass_name = Form("Zprime_Z_mass_M_%d",mass_point[i]);
+
+  c1[i] = new TCanvas(cut_flow_name,"try to show cut flow ",200,10,700,500);   
+  c2[i] = new TCanvas(ele_pT_name,"ele pT distribution ",200,10,700,500);
+  c3[i] = new TCanvas(ll_pT_name,"ll pT distribution ",200,10,700,500);
+  c4[i] = new TCanvas(SD_mass_name,"SD mass distribution ",200,10,700,500);
+  c5[i] = new TCanvas(Z_mass_name,"Z mass distribution ",200,10,700,500);
+
+//  TString ROOT_name = Form("plot_Cut_SD60to140_ElePt85_llPt200/Zprime_shape_M-%d.root",mass_point[i]);
+
+// this will decide the directory where ROOT file to be saved  
+cout<< "dir_flag: " << dir_flag << endl;
+cout<< "dir_name: " << dir_name << endl;
+
+if (dir_flag !=0){
+  xAna_ele( file_name[i] , c1[i] , c2[i] , c3[i] , c4[i] , c5[i] , mass_point[i], eff, eff_err, dir_name , dir_flag );}
+else if (dir_flag ==0){
+  xAna_ele_noCuts( file_name[i] , c1[i] , c2[i] , c3[i] , c4[i] , c5[i] , mass_point[i], eff, eff_err, dir_name);}
+
+
+  TString png1_name = Form("Zprime_Cut_Flow_M_%d.png",mass_point[i]);
+  TString png2_name = Form("Zprime_ele_pT_M_%d.png",mass_point[i]);
+  TString png3_name = Form("Zprime_ll_pT_M_%d.png",mass_point[i]);
+  TString png4_name = Form("Zprime_SD_mass_M_%d.png",mass_point[i]);
+  TString png5_name = Form("Zprime_Z_mass_M_%d.png",mass_point[i]);
+
+png1_name = dir_name + png1_name;
+png2_name = dir_name + png2_name;
+png3_name = dir_name + png3_name;
+png4_name = dir_name + png4_name;
+png5_name = dir_name + png5_name;
+
+
+  c1[i]->cd();  c1[i]->SaveAs(png1_name);
+  c2[i]->cd();  c2[i]->SaveAs(png2_name);
+  c3[i]->cd();  c3[i]->SaveAs(png3_name);
+  c4[i]->cd();  c4[i]->SaveAs(png4_name);
+  c5[i]->cd();  c5[i]->SaveAs(png5_name);
+
+
+
+ }// loop i
+
+}// loop j
+
+
+
+  // ask the match rate  
+
+  // file_name[0] is M-600 ROOT file, 
+  // file_name[2] is M-1000 ROOT file,
+  // file_name[12] is M-4500 ROOT file,
+
+  cout<< "ask the match rate"<<endl;
+  TString dir_name = "plot_TurnOff3Cuts" ; 
+  dir_flag = 1; // N-3 cuts
+  double eff =0, eff_err=0;
+
+
+  int file_index = 12;
+  cout<< "dir_name: " << dir_name <<endl;
+  cout<< "file_name[i]: "<< file_name[file_index] << endl;
+ 
+  xAna_ele_match_rate( file_name[file_index] ,/* c1 , c2 , c3 , c4 , c5 ,*/  mass_point[file_index], eff, eff_err, dir_name , dir_flag );
+
+
+  //  compare ChingWei's SD window efficiency  
+
+  cout<< "compare ChingWei's SD window efficiency  "<<endl;
+//  TString dir_name = "plot_TurnOff3Cuts" ;
+  dir_flag = 1; // N-1 Cut, I will turn all cuts except SD mass
+//  double eff =0, eff_err=0;
+
+
+  file_index = 2;
+  cout<< "dir_name: " << dir_name <<endl;
+  cout<< "file_name[i]: "<< file_name[file_index] << endl;
+
+
+  xAna_ele_compare_ChingWei( file_name[file_index] ,/* c1 , c2 , c3 , c4 , c5 ,*/  mass_point[file_index], eff, eff_err, dir_name , dir_flag );
+
+
+
+
+}
---
+++ 13TeV_Zprime_Electron_channel/back_up_use_TString_for_ROOT_file_name/xAna_ele_compare_ChingWei.C
@@ -0,0 +1,542 @@
+// example code to run Bulk Graviton->ZZ->ZlepZhad selections on electron-channel
+
+// Now Yu-Hsiang change it to Zprime->Zh->ZlepZhad selections on electron-channel
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <TString.h>
+#include <map>
+#include <TH1D.h>
+#include <TFile.h>
+#include "untuplizer.h"
+#include <TClonesArray.h>
+#include <TLorentzVector.h>
+#include <string>
+#include "TCanvas.h"
+
+using namespace std;
+void xAna_ele_compare_ChingWei(/*std::string*/ TString  inputFile, /*TCanvas *c1 , TCanvas *c2 , TCanvas *c3 ,TCanvas *c4 ,TCanvas *c5 , */
+int mass_point ,double eff,double eff_err, TString dir_name, int dir_flag ){
+
+
+  // define histograms
+
+ TString title2 = Form("ele pT for Zprime mass = %d",mass_point);
+ TString title3 = Form("lepton pairs' pT for Zprime mass = %d",mass_point);
+ TString title4 = Form("SD mass for Zprime mass = %d",mass_point);
+ TString title5 = Form("Z mass for Zprime mass = %d",mass_point);
+
+ TH1D* h_ele_pT = new TH1D("h_ele_pT", title2 ,300 , 0,3000 );
+ TH1D* h_lepton_pair_pT = new TH1D("h_lepton_pair_pT", title3 ,400 , 0,4000 );
+// TH1D* h_SD_mass = new TH1D("h_SD_mass", title4 ,100 , 0,1000 );
+ TH1D* h_SD =new TH1D("h_SD",title4 ,100,0,200);
+ TH1D* h_Z_mass= new TH1D("h_Z_mass",title5 ,250,0,500);
+
+  //get TTree from file ...
+//TreeReader data(inputFile.data());
+  TreeReader data(inputFile.Data());
+
+  Long64_t nTotal=0;
+  Long64_t nPass[20]={0};
+  Long64_t nJets_Nminus3Cuts=0;
+  Long64_t nJets_Nminus3Cuts_=0;
+
+  Long64_t nSD20to220=0;
+  Long64_t nSDwindow=0;
+
+
+  ofstream fout;
+  fout.open("ele_Eiko.txt");
+//  TH1F* h_SD=new TH1F("h_SD","",100,0,200);
+
+  //Event loop
+  for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
+
+    if (jEntry % 50000 == 0)
+      fprintf(stderr, "Processing event %lli of %lli\n", jEntry + 1, data.GetEntriesFast());
+
+    data.GetEntry(jEntry);
+    nTotal ++;
+
+    // 0. check the generator-level information and make sure there is a Z->e+e-
+    Int_t nGenPar        = data.GetInt("nGenPar");
+    Int_t* genParId      = data.GetPtrInt("genParId");
+    Int_t* genParSt      = data.GetPtrInt("genParSt");
+    Int_t* genMomParId   = data.GetPtrInt("genMomParId");
+    Int_t* genDa1      = data.GetPtrInt("genDa1");
+    Int_t* genDa2      = data.GetPtrInt("genDa2");
+    TClonesArray* genParP4 = (TClonesArray*) data.GetPtrTObject("genParP4");
+
+    bool hasLepton=false;
+
+    for(int ig=0; ig < nGenPar; ig++){
+
+      if(genParId[ig]!=23)continue;
+      int da1=genDa1[ig];
+      int da2=genDa2[ig];
+
+      if(da1<0 || da2<0)continue;
+      int da1pdg = genParId[da1];
+      int da2pdg = genParId[da2];
+
+      if(abs(da1pdg)==11)
+     	hasLepton=true;
+
+      if(hasLepton)break;
+
+    }
+
+    // 1. make sure there is a h-> bb, Yu-Hsiang change it
+    bool hasHadron=false;
+    std::vector<int> B_quark_from_Higgs_gen_index;
+
+    for(int ig=0; ig < nGenPar; ig++){
+
+      if(genParId[ig]!=25)continue;
+      int da1=genDa1[ig];
+      int da2=genDa2[ig];
+
+      if(da1<0 || da2<0)continue;
+      int da1pdg = genParId[da1];
+      int da2pdg = genParId[da2];
+
+      if(abs(da1pdg)==5)
+     	{hasHadron=true;
+	B_quark_from_Higgs_gen_index.push_back(da1);
+	B_quark_from_Higgs_gen_index.push_back(da2);}
+
+      if(hasHadron)break;
+
+    }
+
+/*    for(unsigned int i=0; i< B_quark_from_Higgs_gen_index.size(); i++){
+       cout<<"i: "<< i <<endl;
+       cout<<"gen index: "<< B_quark_from_Higgs_gen_index[i] <<endl;
+       cout<<"particle IDi: "<< genParId[B_quark_from_Higgs_gen_index[i]] <<endl;
+    }
+*/
+
+
+    
+    if(!hasLepton)continue;
+    nPass[0]++;
+    if(!hasHadron)continue;
+    nPass[1]++;
+     
+    //2. pass electron or muon trigger
+    std::string* trigName = data.GetPtrString("hlt_trigName");
+    vector<bool> &trigResult = *((vector<bool>*) data.GetPtr("hlt_trigResult"));
+    const Int_t nsize = data.GetPtrStringSize();
+
+    bool passTrigger=false;
+    for(int it=0; it< nsize; it++)
+      {
+ 	std::string thisTrig= trigName[it];
+ 	bool results = trigResult[it];
+
+	// std::cout << thisTrig << " : " << results << std::endl;
+	
+ 	if( (thisTrig.find("HLT_Ele105")!= std::string::npos && results==1)
+	    ||
+	    (thisTrig.find("HLT_Mu45")!= std::string::npos && results==1)
+	    )
+ 	  {
+ 	    passTrigger=true;
+ 	    break;
+ 	  }
+
+
+      }
+
+
+    if(!passTrigger)continue;
+    nPass[2]++;
+
+
+    //3. has a good vertex
+    Int_t nVtx        = data.GetInt("nVtx");
+    if(nVtx<1)continue;
+    nPass[3]++;
+
+    //4. look for good electrons first
+    Int_t nEle         = data.GetInt("nEle");
+    Int_t run          = data.GetInt("runId");
+    Int_t lumi         = data.GetInt("lumiSection");
+    Int_t event        = data.GetInt("eventId");
+    vector<bool> &passHEEPID = *((vector<bool>*) data.GetPtr("eleIsPassHEEPNoIso"));
+    TClonesArray* eleP4 = (TClonesArray*) data.GetPtrTObject("eleP4");
+    Float_t* eleSCEta         = data.GetPtrFloat("eleScEta");
+    Float_t* eleMiniIso       = data.GetPtrFloat("eleMiniIso");
+    Int_t*   eleCharge        = data.GetPtrInt("eleCharge");
+
+    //5. select good electrons
+    std::vector<int> goodElectrons;
+
+    for(int ie=0; ie< nEle; ie++)
+      {
+
+    	TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(ie);
+
+    	if(fabs(thisEle->Eta())>2.5)continue;
+
+    	if(! (fabs(eleSCEta[ie])<1.442 || fabs(eleSCEta[ie])>1.566))continue;
+
+	double ele_pt_threshold = -999;
+//	if (dir_flag ==1){ele_pt_threshold = 0;}
+//        if (dir_flag ==2 || dir_flag ==3 || dir_flag ==6 ){ele_pt_threshold = 85;}
+//        if (dir_flag ==4){ele_pt_threshold = 100;}
+//        if (dir_flag ==5){ele_pt_threshold = 115;}
+//        if (dir_flag ==7 || dir_flag ==8 || dir_flag ==9 ){ele_pt_threshold = 115;}
+
+
+//    	if(thisEle->Pt() < ele_pt_threshold )continue;
+     	  if(thisEle->Pt() < 115)continue;
+//        if(thisEle->Pt() < 100)continue;
+//        if(thisEle->Pt() < 85)continue;
+
+    	if(!passHEEPID[ie])continue;
+    	
+    	if(eleMiniIso[ie]>0.1)continue;
+
+    	goodElectrons.push_back(ie);
+      }
+
+	
+    //6. select a good Z boson
+    bool findEPair=false;
+    TLorentzVector l4_Z(0,0,0,0);
+    std::vector<double> LeptonPairPt;
+    std::vector<double> LeptonPairM;
+
+    for(unsigned int i=0; i< goodElectrons.size(); i++)
+      {
+	int ie = goodElectrons[i];
+	TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(ie);
+
+	for(unsigned int j=0; j< i; j++)
+	  {
+	    int je= goodElectrons[j];
+
+	    if(eleCharge[ie]*eleCharge[je]>0)continue;
+
+	    TLorentzVector* thatEle = (TLorentzVector*)eleP4->At(je);
+
+	    Float_t mll  = (*thisEle+*thatEle).M();
+	    Float_t ptll = (*thisEle+*thatEle).Pt();
+	    
+
+	    if(mll<70 || mll>110)continue;
+
+//            double ll_pt_threshold = -999;
+//            if (dir_flag ==1){ll_pt_threshold = 0;}
+//            if (dir_flag ==2 ||dir_flag ==3 ||dir_flag ==4 ||dir_flag ==5 ||dir_flag ==6 ){ll_pt_threshold = 200;}
+
+//          if(ptll<ll_pt_threshold )continue;
+	    if(ptll<200)continue;
+
+
+            LeptonPairPt.push_back(ptll);
+            LeptonPairM.push_back(mll);
+
+	    if(!findEPair)l4_Z=(*thisEle+*thatEle);
+
+	    findEPair=true;
+	  }	
+      }
+
+    if(!findEPair)
+      continue;
+    nPass[4]++;
+
+
+    //7.select a good CA8 and cleaned jet
+
+    // first select muons for cleaning against jet
+    std::vector<int> goodMuons;
+    Int_t nMu          = data.GetInt("nMu");
+    vector<bool> &isHighPtMuon = *((vector<bool>*) data.GetPtr("isHighPtMuon"));
+    vector<bool> &isCustomTrackerMuon = *((vector<bool>*) data.GetPtr("isCustomTrackerMuon"));
+    TClonesArray* muP4 = (TClonesArray*) data.GetPtrTObject("muP4");
+    Float_t* muMiniIso       = data.GetPtrFloat("muMiniIso");
+
+    for(int im=0; im< nMu; im++)
+      {
+
+    	TLorentzVector* thisMu = (TLorentzVector*)muP4->At(im);
+
+    	if(!isHighPtMuon[im] && !isCustomTrackerMuon[im])continue;
+    	
+    	if(muMiniIso[im]>0.1)continue;	
+
+        if ( goodMuons.size()==1 ) {
+	  bool highPt_AND_tracker = isHighPtMuon[0] && isCustomTrackerMuon[im];
+	  bool tracker_AND_highPt = isHighPtMuon[im] && isCustomTrackerMuon[0]; 
+            if ( !(highPt_AND_tracker or tracker_AND_highPt) ) continue; 
+        }
+
+    	if(fabs(thisMu->Eta())>2.1)continue;
+
+    	if(thisMu->Pt() < 50)continue;
+
+    	goodMuons.push_back(im);
+      }
+
+	
+    Int_t nJet         = data.GetInt("FATnJet");
+    TClonesArray* jetP4 = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+    Float_t*  jetSDmass = data.GetPtrFloat("FATjetSDmass");
+
+    std::vector<double> SD_Mass;
+    std::vector<int> SD_jet_index;
+    TLorentzVector l4_leadingJet(0,0,0,0);
+    bool findAJet=false;
+    for(int ij=0; ij<nJet; ij++)
+      {
+    	
+     	TLorentzVector* thisJet = (TLorentzVector*)jetP4->At(ij);
+
+
+        double SD_low = -999, SD_high = 999;
+        if (dir_flag ==1){ SD_low= 0; SD_high = 9999;}
+//        if (dir_flag ==2){ SD_low= 40; SD_high = 140;}
+//        if (dir_flag ==3){ SD_low= 50; SD_high = 140;}
+//        if (dir_flag ==4 || dir_flag ==5 || dir_flag ==6){ SD_low= 60; SD_high = 140;}
+
+//        if (dir_flag ==7){ SD_low= 93; SD_high = 143;}
+//        if (dir_flag ==8){ SD_low= 95; SD_high = 145;}
+//        if (dir_flag ==9){ SD_low= 91; SD_high = 141;}
+
+        if(jetSDmass[ij]<SD_low || jetSDmass[ij]>SD_high)continue;
+//	if(jetSDmass[ij]<50 || jetSDmass[ij]>110)continue; // this is soft drop mass ?
+//      if(jetSDmass[ij]<105 || jetSDmass[ij]>145)continue;  // Yu-Hsiang change to Higgs mass window
+//      if(jetSDmass[ij]<60 || jetSDmass[ij]>140)continue;  // Yu-Hsiang change to Higgs mass window
+//      if(jetSDmass[ij]<50 || jetSDmass[ij]>140)continue;  // Yu-Hsiang change to Higgs mass window
+//      if(jetSDmass[ij]<40 || jetSDmass[ij]>140)continue;  // Yu-Hsiang change to Higgs mass window
+
+	bool hasOverLap=false;
+	for(unsigned int ie=0; ie < goodElectrons.size(); ie++)
+	  {
+	    TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(goodElectrons[ie]);
+	    if(thisEle->DeltaR(*thisJet)<0.8)hasOverLap=true;
+	    if(hasOverLap)break;
+	    
+	  }
+	for(unsigned int im=0; im < goodMuons.size(); im++)
+	  {
+	    TLorentzVector* thisMuo = (TLorentzVector*)muP4->At(goodMuons[im]);
+	    if(thisMuo->DeltaR(*thisJet)<0.8)hasOverLap=true;
+	    if(hasOverLap)break;
+	    
+	  }
+	
+	if(hasOverLap)continue;
+	
+	if(thisJet->Pt()<200)continue;
+	if(fabs(thisJet->Eta())>2.4)continue;
+
+     	if(!findAJet)
+	  {
+	    l4_leadingJet = *thisJet;
+//	    h_SD->Fill(jetSDmass[ij]);
+//            SD_Mass.push_back( jetSDmass[ij] );
+	  }
+	    
+     	findAJet=true;
+        SD_Mass.push_back( jetSDmass[ij] ); // change to this place so that loop to all jets passing cuts    	
+	SD_jet_index.push_back( ij );
+        nJets_Nminus3Cuts ++;// count # of jets passing N-3 cuts
+      }
+    
+    if(!findAJet)
+      continue;
+    nPass[5]++;
+
+     Float_t MGrav = (l4_leadingJet + l4_Z).M();
+     if(MGrav<400)continue;
+     nPass[6]++;
+
+  // if event can go here, then fill the histograms to plot the distributions. Yu-Hsiang add 
+
+    for(unsigned int i=0; i< goodElectrons.size(); i++)
+      {         int ie = goodElectrons[i];
+		TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(ie);
+		h_ele_pT->Fill( thisEle->Pt() );
+      }
+
+    for(unsigned int i=0; i< LeptonPairPt.size(); i++)
+      {         
+                
+                h_lepton_pair_pT->Fill( LeptonPairPt[i] );
+      }
+
+    for(unsigned int i=0; i< LeptonPairM.size(); i++)
+      {
+
+                h_Z_mass->Fill( LeptonPairM[i] );
+      }
+
+    for(unsigned int i=0; i< SD_Mass.size(); i++)
+      {
+
+                h_SD->Fill( SD_Mass[i] );
+                nJets_Nminus3Cuts_++;
+      }
+
+  //////// compare ChingWei 's SD window efficiency 
+
+//    double SD_low =91, SD_high =141;
+//    double SD_low =95, SD_high =145;
+    double SD_low =93, SD_high =143;
+
+    for(unsigned int i=0; i< SD_jet_index.size(); i++){
+        
+      if(SD_Mass[i]<20 || SD_Mass[i]>220 )continue;
+      nSD20to220 ++;
+
+      if(SD_Mass[i]<SD_low || SD_Mass[i]>SD_high )continue;
+      nSDwindow ++;
+
+}
+  ////////
+
+    fout << run << " " << lumi << " " << event << endl;
+    
+
+  } // end of loop over entries
+
+  fout.close();
+  std::cout << "nTotal    = " << nTotal << std::endl;
+  for(int i=0;i<20;i++)
+    if(nPass[i]>0)
+      std::cout << "nPass[" << i << "]= " << nPass[i] << std::endl;
+
+
+
+
+  //  Yu-Hsiang add calulation of total efficiency and eff uncertainty
+
+
+  double pass =-99,fail=-99,f_over_p=-99,f_over_p_error=-99;
+//  double n_total = nTotal;// from int to double
+  double n_total = nPass[1];// from int to double
+
+eff = nPass[6]/n_total;
+pass = nPass[6];
+fail = nTotal -  nPass[6];
+f_over_p = fail/pass;
+f_over_p_error = f_over_p * sqrt( (1/fail) + (1/pass) );
+eff_err = f_over_p_error/pow( 1 + f_over_p ,2);
+
+//cout<<"eff: "<< eff << "   eff_err: "<< eff_err <<endl;
+
+//cout<< "nJets_Nminus3Cuts: "<< nJets_Nminus3Cuts <<endl;
+//cout<< "nJets_Nminus3Cuts_: "<< nJets_Nminus3Cuts_ <<endl;
+
+
+cout<< "nSD20to220: "<<nSD20to220 <<endl;
+cout<< "nSDwindow: "<<nSDwindow <<endl;
+
+double NSD20to220= nSD20to220;
+double NSDwindow = nSDwindow;
+
+double SD_window_eff = NSDwindow/NSD20to220;
+cout<< "SD_window_eff: "<< SD_window_eff <<endl;
+
+  // Yu-Hsiang add cut flow figure
+
+
+  TString title1 = Form("Cut Flow for Zprime mass = %d, eff=%f +/- %f",mass_point,eff,eff_err);
+
+  TH1D* h_CutFlow = new TH1D("h_CutFlow", title1 ,8 , 0,8 );
+
+  char* cut_name[8] = {"Began","Z->ee in Gen","H->bb in Gen","HLT","Vertex","Leptons","V-jet","Zprime mass"};
+
+	for(int i=1;i<=8;i++){ // i is the index of column of cut flow plot 
+		if(i==1) {h_CutFlow->SetBinContent(i,nTotal); }
+        	else {h_CutFlow->SetBinContent(i,nPass[i-2]); }
+		h_CutFlow->GetXaxis()->SetBinLabel( i , cut_name[i-1] );
+	}
+
+//
+
+  TString png1_name = Form("Zprime_Cut_Flow_M_%d.png",mass_point);
+  TString png2_name = Form("Zprime_ele_pT_M_%d.png",mass_point);
+  TString png3_name = Form("Zprime_ll_pT_M_%d.png",mass_point);
+  TString png4_name = Form("Zprime_SD_mass_M_%d.png",mass_point);
+  TString png5_name = Form("Zprime_Z_mass_M_%d.png",mass_point);
+
+
+
+//   TCanvas *c1 = new TCanvas("c1","try to show cut flow ",200,10,700,500);
+  //c1->cd();
+/*
+  gPad->SetGridx();
+//  gPad->SetLogy();
+
+  h_CutFlow->SetMarkerStyle(8);
+  h_CutFlow->SetMarkerSize(1);
+  h_CutFlow->GetXaxis()->SetLabelSize(0.041);
+//  h_CutFlow->GetYaxis()->SetLabelSize(0.035);
+
+  h_CutFlow->SetStats(0);
+  h_CutFlow->SetMarkerSize(2.0);
+
+ // h_CutFlow->Draw();
+ // h_CutFlow->Draw("HIST TEXT0 SAME");
+
+*/
+//
+    // Yu-Hsiang add drawing histogram of distributuion
+/*
+  c2->cd();
+  h_ele_pT->Draw();
+
+  c3->cd();
+  h_lepton_pair_pT->Draw();
+
+  c4->cd();
+  h_SD->Draw();
+
+  c5->cd();
+  h_Z_mass->Draw();
+*/
+////
+
+   // Yu-Hsiang add that save TH1D in the ROOT file
+
+
+//   TString ROOT_name = Form("test_ROOT_name_M-%d.root",mass_point);
+//   TString ROOT_name = Form("plot_TurnOff3Cuts/Zprime_shape_M-%d.root",mass_point);
+
+//   TString ROOT_name = Form("plot_Cut_SD40to140_ElePt85_llPt200/Zprime_shape_M-%d.root",mass_point);
+//   TString ROOT_name = Form("plot_Cut_SD50to140_ElePt85_llPt200/Zprime_shape_M-%d.root",mass_point);
+//   TString ROOT_name = Form("plot_Cut_SD60to140_ElePt100_llPt200/Zprime_shape_M-%d.root",mass_point);
+//   TString ROOT_name = Form("plot_Cut_SD60to140_ElePt115_llPt200/Zprime_shape_M-%d.root",mass_point);
+//   TString ROOT_name = Form("plot_Cut_SD60to140_ElePt85_llPt200/Zprime_shape_M-%d.root",mass_point);
+
+/*
+   TString ROOT_name = Form("Zprime_shape_M-%d.root",mass_point);
+   ROOT_name = dir_name + ROOT_name; 
+
+   TFile *myFile = new TFile(ROOT_name,"recreate");
+
+   h_CutFlow->Write();
+   h_ele_pT->Write();
+   h_lepton_pair_pT->Write();
+   h_SD->Write();
+   h_Z_mass->Write();
+
+   myFile->Close();
+   delete myFile;
+*/
+
+   // delete the finished used TH1D so it will not replacing the existing TH1 (no potential memory leak warning) when loop again 
+/*
+   delete h_CutFlow;
+   delete h_ele_pT;
+   delete h_lepton_pair_pT;
+   delete h_SD;
+   delete h_Z_mass;
+*/
+}
