7e927387c6786ad2fd26530c8094b38d5c8d137f
Yu-hsiang <index0192@yahoo.com.tw>
2015-10-01 18:42:14 +0200

    [comment]

---
+++ 13TeV_Zprime_Electron_channel/ToGithub_selection_with_ZtoEE_header_file/ElectronSelection.h
@@ -0,0 +1,109 @@
+
+Float_t geteIDEA(Int_t i, Float_t SCEta) {
+
+  Float_t EffectiveArea = 0;
+
+  if (fabs(SCEta) >= 0.0 && fabs(SCEta) < 1.0 ) EffectiveArea = 0.013 + 0.122;
+  if (fabs(SCEta) >= 1.0 && fabs(SCEta) < 1.479 ) EffectiveArea = 0.021 + 0.147 ;
+  if (fabs(SCEta) >= 1.479 && fabs(SCEta) < 2.0 ) EffectiveArea = 0.013 + 0.055;
+  if (fabs(SCEta) >= 2.0 && fabs(SCEta) < 2.2 ) EffectiveArea = 0.010 + 0.106;
+  if (fabs(SCEta) >= 2.2 && fabs(SCEta) < 2.3 ) EffectiveArea = 0.024 + 0.138;
+  if (fabs(SCEta) >= 2.3 && fabs(SCEta) < 2.4 ) EffectiveArea = 0.020 + 0.221;
+  if (fabs(SCEta) >= 2.4 ) EffectiveArea = 0.019 + 0.211;
+
+  return EffectiveArea;
+}
+
+// HEEP v5.1 ID
+void select_electrons(TreeReader &data, vector<int> *accepted) {
+
+  accepted->clear();
+
+  // load relevant branches from TTree/TChain
+  Int_t    nEle             = data.GetInt("nEle");
+  TClonesArray* eleP4 = (TClonesArray*) data.GetPtrTObject("eleP4");
+  Float_t* eleSCEta         = data.GetPtrFloat("eleScEta");
+  Float_t* eleSCEn         = data.GetPtrFloat("eleScEn");
+
+  vector<bool> & eleEcalDrivenSeed = *((vector<bool>*) data.GetPtr("eleEcalDrivenSeed"));
+  Float_t* eleEtaseedAtVtx  = data.GetPtrFloat("eleEtaseedAtVtx");
+  Float_t* eledPhiAtVtx     = data.GetPtrFloat("eledPhiAtVtx");
+
+  Float_t* eleHoverE        = data.GetPtrFloat("eleHoverE");
+
+  Float_t* eleE1x5Full5x5   = data.GetPtrFloat("eleE1x5Full5x5");
+  Float_t* eleE2x5Full5x5   = data.GetPtrFloat("eleE2x5Full5x5");
+  Float_t* eleE5x5Full5x5   = data.GetPtrFloat("eleE5x5Full5x5");
+
+  Float_t* eleDr03EcalRecHitSumEt = data.GetPtrFloat("eleDr03EcalRecHitSumEt");
+  Float_t* eleDr03HcalDepth1TowerSumEt = data.GetPtrFloat("eleDr03HcalDepth1TowerSumEt");
+  Float_t* eleDr03TkSumPt= data.GetPtrFloat("eleDr03TkSumPt");
+
+  Float_t* eleSigmaIEtaIEta = data.GetPtrFloat("eleSigmaIEtaIEta");
+  Int_t*   eleMissHits      = data.GetPtrInt("eleMissHits");
+  Float_t* eleD0            = data.GetPtrFloat("eleD0");
+  Float_t  rho              = data.GetFloat("eleRho");
+  Float_t* eleMiniIso       = data.GetPtrFloat("eleMiniIso");
+
+
+  // loop over lepton candidates
+  for (int i = 0; i < nEle; ++i) {
+
+    Float_t  scEta=eleSCEta[i];
+
+    if(! (fabs(scEta) < 1.4442) &&
+       ! (fabs(scEta) < 2.5   &&
+	  fabs(scEta) > 1.566)
+       )continue;
+
+
+    if(! (fabs(scEta)<1.442 || fabs(scEta)>1.566))continue;
+    
+    TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(i);
+    Float_t Et = thisEle->Et();
+    Float_t E = eleSCEn[i];
+
+    if(fabs(thisEle->Eta())>2.5)continue;
+    
+//    if(thisEle->Pt()<115)continue;
+
+//    if(Et < 35)continue;
+
+    if(!eleEcalDrivenSeed[i])continue;
+
+//    if(eleMiniIso[i]>0.1)continue; // I cut the Mini-Iso still in other place 
+    
+//    Float_t caliso = eleDr03EcalRecHitSumEt[i]+ 
+//      eleDr03HcalDepth1TowerSumEt[i];
+
+    
+    if (fabs(scEta) < 1.4442) {
+      if (fabs(eleEtaseedAtVtx[i])>0.004)continue;
+      if (fabs(eledPhiAtVtx[i])>0.06)continue;
+      if (eleHoverE[i] > (1/E + 0.05))continue;
+
+      if( eleE2x5Full5x5[i]/eleE5x5Full5x5[i] < 0.94 && eleE1x5Full5x5[i]/eleE5x5Full5x5[i] < 0.83)continue;
+      
+
+      if (eleMissHits[i] > 1) continue;
+      if (fabs(eleD0[i])>0.02)continue;
+    }
+   else {
+      if (fabs(eleEtaseedAtVtx[i])>0.006)continue;
+      if (fabs(eledPhiAtVtx[i])>0.06)continue;
+      if (eleHoverE[i] > (5/E + 0.05))continue;
+
+      if (eleSigmaIEtaIEta[i] > 0.03) continue;
+      
+      if (eleMissHits[i] > 1) continue;
+      if (fabs(eleD0[i])>0.05)continue;
+
+
+   }
+    accepted->push_back(i);
+  }
+
+}
+
+
+
---
+++ 13TeV_Zprime_Electron_channel/ToGithub_selection_with_ZtoEE_header_file/ZtoEEfinder.h
@@ -0,0 +1,146 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <TH1D.h>
+#include <TH1F.h>
+#include <TMath.h>
+#include <TLorentzVector.h>
+#include "untuplizer.h"
+//#include "ElectronSelection.h"
+
+using namespace std;
+
+int ZtoEEfinder(TreeReader &, vector<Int_t> *);
+//void select_electrons(TreeReader &, vector<int> * );
+
+int ZtoEEfinder(TreeReader &data, vector<Int_t> *Z_electron_index)
+{
+
+  // test
+  //  for(int i=0;i<5;i++){
+
+  //	Z_electron_index->push_back( i+2 );
+  //}
+
+    Int_t nEle         = data.GetInt("nEle");
+    TClonesArray* eleP4 = (TClonesArray*) data.GetPtrTObject("eleP4");
+    Float_t* eleSCEta         = data.GetPtrFloat("eleScEta");
+    Float_t* eleSCEt          = data.GetPtrFloat("eleScEt");
+    Float_t* eleMiniIso       = data.GetPtrFloat("eleMiniIso");
+    Int_t*   eleCharge        = data.GetPtrInt("eleCharge");
+
+
+    //5. select good electrons
+
+    // save index of electron accepted by ElectronSelection.h
+    std::vector<int> accepted;
+    select_electrons(data, &accepted);
+
+    std::vector<int> goodElectrons;
+
+
+    for(int ie=0; ie< nEle; ie++)
+      {
+
+        TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(ie);
+
+        if(fabs(thisEle->Eta())>2.5)continue;
+
+        if(! (fabs(eleSCEta[ie])<1.442 || fabs(eleSCEta[ie])>1.566))continue;
+
+        float ele_pt = thisEle->Pt();
+
+        bool has_accepted = false;
+        for(int j=0; j< accepted.size();j++)
+                { if(ie == accepted[j]){has_accepted=true; break;}  }
+        if(!has_accepted)continue;
+
+//        cout<<"after accepted" <<endl;
+//        cout<<"ele_pt: "<< ele_pt <<endl;
+
+        if(eleMiniIso[ie]>0.1)continue;
+
+        goodElectrons.push_back(ie);
+      }
+
+
+    //6. select a good Z boson
+    bool findEPair=false;
+    TLorentzVector l4_Z(0,0,0,0);
+//    std::vector<double> LeptonPairPt;
+//    std::vector<double> LeptonPairM;
+//    std::vector<int> leading_ele_index;
+//    std::vector<int> subleading_ele_index;
+
+
+    for(unsigned int i=0; i< goodElectrons.size(); i++)
+      {
+        int ie = goodElectrons[i];
+        TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(ie);
+
+        for(unsigned int j=0; j< i; j++)
+          {
+            int je= goodElectrons[j];
+
+            if(eleCharge[ie]*eleCharge[je]>0)continue;
+
+
+            TLorentzVector* thatEle = (TLorentzVector*)eleP4->At(je);
+
+            float pt1 = thisEle->Pt();// use pt
+            float pt2 = thatEle->Pt(); 
+
+
+            float ptmax = TMath::Max(pt1,pt2);
+            float ptmin = TMath::Min(pt1,pt2);
+            if(ptmax<115)continue;
+//            if(ptmin<35)continue;
+            if(ptmin<35)continue;
+
+            int leadingIndex = pt1>pt2? ie : je;
+            int subleadingIndex = pt1 > pt2? je : ie;
+
+            Float_t ptll = (*thisEle+*thatEle).Pt();
+            Float_t mll  = (*thisEle+*thatEle).M();
+            if(mll<70 || mll>110)continue;
+//          if(ptll<100)continue;
+//            if(ptll<120)continue;
+            if(ptll<200)continue;
+
+//            leading_ele_index.push_back(leadingIndex);
+//            subleading_ele_index.push_back(subleadingIndex);
+ 	    Z_electron_index->push_back( leadingIndex  );
+            Z_electron_index->push_back( subleadingIndex  );
+
+//            LeptonPairPt.push_back(ptll);
+//            LeptonPairM.push_back(mll);
+
+//            Float_t Phi_ll = (*thisEle+*thatEle).Phi();
+//            Float_t Eta_ll = (*thisEle+*thatEle).Eta();
+
+//          cout<<" Phi_ll: "<< Phi_ll <<endl;
+//          cout<<" Eta_ll: "<< Eta_ll <<endl;
+
+            if(!findEPair)l4_Z=(*thisEle+*thatEle);
+
+            findEPair=true;
+          }
+      }
+
+    if(findEPair){ return 1;}
+    else if(!findEPair){ return 0;}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
