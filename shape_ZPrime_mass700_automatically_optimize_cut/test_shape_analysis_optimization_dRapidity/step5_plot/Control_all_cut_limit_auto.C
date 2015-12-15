4a265172e1fbac4b308b8d8abf63b2f0504eb42c
Yu-hsiang <index0192@yahoo.com.tw>
2015-12-15 09:45:45 +0100

    [comment]

---
+++ shape_ZPrime_mass700_automatically_optimize_cut/test_shape_analysis_optimization_dRapidity/step5_plot/Control_all_cut_limit_auto.C
@@ -0,0 +1,160 @@
+#include <Riostream.h>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <cstdlib>
+#include "TTree.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include "TROOT.h"
+#include "TGraphAsymmErrors.h"
+#include "TCanvas.h"
+#include "TLine.h"
+#include "TLatex.h"
+#include "TLegend.h"
+#include "TStyle.h"
+#include "TPaveText.h"
+#include "plot_Asymptotic.C"
+#include "TString.h"
+
+using namespace std;
+
+void Control_all_cut_limit_auto()
+{
+
+const float intLumi = 3.0;
+const int n_cut_index =10; // 10
+
+  TString variable_name = "Abs_dRapidity_ZH";// "Abs_dEta_ZH" ;
+  cout<<"variable_name: " << variable_name << endl;
+
+  // define cuts
+  
+  double cut_value[n_cut_index];
+  double lowest_cut=0.5 ;
+  double scan_width = 0.5;// 21
+
+  for(int i=0;i<n_cut_index ;i++){
+        cut_value[i] = lowest_cut + scan_width*i;
+        cout<<"cut_value[i]: "<< cut_value[i] << endl;
+  }
+  //
+
+//int cut_value[n_cut_index] = 
+//{115,136,157,178,199,220,241,262,283,304};
+
+  TGraphAsymmErrors *grmedian_cls;
+  TGraphAsymmErrors *grmedian_cls_new[n_cut_index];
+
+  const int nMassEff = 10;
+  double mass[nMassEff] = {0};
+  double medianD[nMassEff] = {0};
+
+
+  grmedian_cls = new TGraphAsymmErrors(nMassEff, mass, medianD);
+
+
+//  TString dir_path = "Datacards_txt_files/YuHsiang_DY_h_ele_1st_pT_cut_115/";
+  TString dir_path[n_cut_index] ;
+  TString leg_name[n_cut_index] ;
+
+
+
+  for(int i=0;i<n_cut_index;i++){
+//  for(int i=0;i<5;i++){
+
+  dir_path[i] = Form("_cut_%f/",cut_value[i] ) ;
+  dir_path[i] =  "YuHsiang_DY_" + variable_name +  dir_path[i];
+//  dir_path[i] = Form("YuHsiang_DY_h_ele_1st_pT_cut_%d/",cut_value[i] ) ;
+  dir_path[i] = "Datacards_text_and_ROOT_files/" + dir_path[i];
+//  dir_path[i] = "Datacards_txt_files/" + dir_path[i];
+   
+  grmedian_cls_new[i] =  plot_Asymptotic( dir_path[i] ,grmedian_cls );
+
+//  leg_name[i] = Form("CL_{S} Expected Limit for cut pt>%d",cut_value[i] ) ;
+  leg_name[i] = Form("|#Delta Rapidity_{ZH}| <%f",cut_value[i] ) ;
+  }
+
+
+
+
+
+  TCanvas *c1 = new TCanvas("c1", "canvas with limits for Asymptotic CLs", 630, 600);
+
+
+  TLegend *leg = new TLegend(.65, .50, 1.0, .90);
+  leg->SetFillColor(0);
+  leg->SetShadowColor(0);
+  leg->SetTextFont(42);
+  leg->SetTextSize(0.03);
+
+  c1->SetGridx(1);
+  c1->SetGridy(1);
+    gPad->SetLogy();
+
+
+  
+  c1->cd();
+
+
+   double fr_left = 500.0, fr_down = 7E-2, fr_right = 4500.0, fr_up = 2E0;
+  TH1F *hr = c1->DrawFrame(fr_left, fr_down, fr_right, fr_up, "");
+
+  int colorN = 1;
+
+// loop
+
+  for(int i=0;i<n_cut_index;i++){
+
+	if (i==0){colorN = kRed;}
+        if (i==3){colorN = kOrange;}
+        if (i==6){colorN = kYellow;}
+        if (i==9){colorN = kCyan;}
+        if (i==12){colorN = kBlue;}
+	
+  	grmedian_cls_new[i]->SetLineColor(colorN);
+        grmedian_cls_new[i]->SetLineStyle(1);
+  	grmedian_cls_new[i]->Draw();
+
+//  	leg->AddEntry(grmedian_cls_new[i], "CL_{S} Expected Limit", "L");
+        leg->AddEntry(grmedian_cls_new[i],leg_name[i] , "L");
+  	leg->Draw();
+
+	colorN = colorN+1;
+  }
+// end loop
+
+  hr->SetXTitle("M_{Z'} [GeV]");
+  hr->SetYTitle("95% #sigma_{Z'}xBR(Z'->ZH) [pb]");
+
+    TLatex * latex = new TLatex();
+    latex->SetNDC();
+    latex->SetTextSize(0.04);
+    latex->SetTextAlign(31);
+    latex->SetTextAlign(11);
+
+    latex->DrawLatex(0.15, 0.91, "CL_{S} Expected Limit, cut |#Delta Rapidity_{ZH}|");
+    latex->DrawLatex(0.18, 0.96, "CMS preliminary 2015");
+    latex->DrawLatex(0.60, 0.96, Form("%.1f fb^{-1} at #sqrt{s} = 13 TeV", intLumi));
+
+
+  TString save_name = "expected_limit_cut_";
+  save_name = save_name + variable_name ;
+
+  TString png_name = save_name + ".png";
+  TString pdf_name = save_name + ".pdf";
+
+  c1->SaveAs(png_name);
+  c1->SaveAs(pdf_name);
+
+
+TString path_to_save = "/afs/cern.ch/user/y/yuchang/www/" ;
+pdf_name = path_to_save + pdf_name;
+
+c1->SaveAs( pdf_name );
+
+
+}// end codes
+
+
---
+++ shape_ZPrime_mass700_automatically_optimize_cut/test_shape_analysis_optimization_leading_pt/backup_2015_12_3/step1_apply_PreSelection_save_TTree/backup/Control_auto.C
@@ -0,0 +1,253 @@
+#include <iostream>
+#include <fstream>
+#include <TString.h>
+#include <TH1D.h>
+#include <TFile.h>
+#include <string>
+#include "TCanvas.h"
+#include "xAna_ele_selection.C"
+
+void Control_auto(){
+
+int signal_background_flag =-1;
+
+
+
+const int N_mass_points = 13;
+
+/*
+TString file_name[N_mass_points] = {
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-600_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-800_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-1000_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-1200_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-1400_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-1600_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-1800_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-2000_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-2500_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-3000_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-3500_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-4000_13TeV-madgraph.root",
+"ZprimeToZhToZlephbb_25ns/ZprimeToZhToZlephbb_narrow_M-4500_13TeV-madgraph.root"
+                        };
+*/
+/*
+TString file_name[N_mass_points] = {
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-600_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-800_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-1000_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-1200_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-1400_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-1600_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-1800_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-2000_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-2500_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-3000_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-3500_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-4000_13TeV-madgraph",
+"ZPrimeZH/ZprimeToZhToZlephbb_narrow_M-4500_13TeV-madgraph"
+                        };
+*/
+TString file_name[N_mass_points] = {
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-600_13TeV-madgraph",
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-800_13TeV-madgraph",
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-1000_13TeV-madgraph",
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-1200_13TeV-madgraph",
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-1400_13TeV-madgraph",
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-1600_13TeV-madgraph",
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-1800_13TeV-madgraph",
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-2000_13TeV-madgraph"
+,"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-2500_13TeV-madgraph"
+,
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-3000_13TeV-madgraph"
+,
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-3500_13TeV-madgraph"
+,
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-4000_13TeV-madgraph"
+,
+"ZprimeToZhToZlephbb_ReMiniAOD/ZprimeToZhToZlephbb_narrow_M-4500_13TeV-madgraph"
+                        };
+
+const int N_background_points = 4;
+
+/*
+TString background_file_name[N_background_points] = {
+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_25ns/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root",
+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_25ns/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root",
+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_25ns/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root",
+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_25ns/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root"
+                        };
+*/
+
+/*
+TString background_file_name[N_background_points] = {
+"DYJetsLLHT/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+"DYJetsLLHT/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+"DYJetsLLHT/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+"DYJetsLLHT/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"
+                        };
+*/
+
+TString background_file_name[N_background_points] = {
+"DYJetsToLL_M-50_HT_ReMiniAOD/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+"DYJetsToLL_M-50_HT_ReMiniAOD/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+"DYJetsToLL_M-50_HT_ReMiniAOD/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+"DYJetsToLL_M-50_HT_ReMiniAOD/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"
+                        };
+
+
+
+
+//TString path = "/data7/khurana/NCUGlobalTuples/SPRING15_2015_10_12/";
+TString path = "/data7/khurana/NCUGlobalTuples/SPRING15_ReMiniAODSIM/";
+
+for(int i=0;i<N_mass_points;i++)
+{ file_name[i] = path + file_name[i];      }
+
+for(int i=0;i<N_background_points;i++)
+{ background_file_name[i] = path + background_file_name[i];      }
+
+
+
+  int mass_point[N_mass_points]={600,800,1000,1200,1400,1600,1800,2000,2500,3000,3500,4000,4500};
+
+TCanvas *c1[N_mass_points];
+TCanvas *c2[N_mass_points];
+TCanvas *c3[N_mass_points];
+TCanvas *c4[N_mass_points];
+TCanvas *c5[N_mass_points];
+TCanvas *c6[N_mass_points];
+
+int dir_flag =0;// choose which Cut you want
+
+
+
+
+ // sub_leading electron pT optimize study
+
+bool sub_leading_ele_flag= false; 
+sub_leading_ele_flag=true;
+
+if(sub_leading_ele_flag){
+
+// now we don't use HEEPNoIso flag, but use ElectronSelection instead
+  dir_flag = 1; // use passHEEPID
+//  dir_flag = 2; // use passLoose
+
+
+//  signal_background_flag = 0; // Zprime signal
+// signal_background_flag = 1; // DYJets_HT background
+
+
+
+for (int signal_background_index =0; signal_background_index<2; signal_background_index++  ){// signal_background_flag
+signal_background_flag = signal_background_index ;
+//if( signal_background_index!= 0 )continue;// run signal sample only
+if( signal_background_index!= 1 )continue;// run bkg sample only
+
+
+  for(int file_index=0;file_index<13;file_index++){// which signal mass points you want ?
+//if( file_index!= 2 && file_index!= 7 && file_index!= 9 )continue;
+//if( file_index!= 2 )continue;
+
+  TString dir_name;
+//  if(dir_flag==1){dir_name = "plot_SubLedaing_ele_pt_optimize/" ;}
+  if(dir_flag==1){dir_name = "plot/" ;}
+
+
+  double eff =0, eff_err=0;
+
+  int i = file_index;// i = file_index for convenience
+
+  TString cut_flow_name = Form("Zprime_Cut_Flow_M_%d",mass_point[i]);
+  TString ele_pT_name = Form("Zprime_ele_pT_M_%d",mass_point[i]);
+  TString ll_pT_name = Form("Zprime_ll_pT_M_%d",mass_point[i]);
+  TString SD_mass_name = Form("Zprime_SD_mass_M_%d",mass_point[i]);
+  TString Z_mass_name = Form("Zprime_Z_mass_M_%d",mass_point[i]);
+  TString Zprime_mass_name = Form("Zprime_Zprime_mass_M_%d",mass_point[i]);
+
+  c1[i] = new TCanvas(cut_flow_name,"try to show cut flow ",200,10,700,500);
+  c2[i] = new TCanvas(ele_pT_name,"ele pT distribution ",200,10,700,500);
+  c3[i] = new TCanvas(ll_pT_name,"ll pT distribution ",200,10,700,500);
+  c4[i] = new TCanvas(SD_mass_name,"SD mass distribution ",200,10,700,500);
+  c5[i] = new TCanvas(Z_mass_name,"Z mass distribution ",200,10,700,500);
+  c6[i] = new TCanvas(Zprime_mass_name,"Zprime mass distribution ",200,10,700,500);
+
+
+
+
+  if(signal_background_flag == 0 )// read signal sample
+      { 
+
+  	cout<< "dir_name: " << dir_name <<endl;
+  	cout<< "file_index: "<< file_index << endl;
+  	cout<< "file_name[file_index]: "<< file_name[file_index] << endl;
+  	cout<< "mass_point[file_index]: "<< mass_point[file_index]<<endl;
+  	cout<< "dir_flag: "<< dir_flag <<endl;
+
+  	int background_file_index = -1;// not use background  
+
+	xAna_ele_selection(file_name[file_index] , c1[file_index] , c2[file_index] , c3[file_index] , c4[file_index] , c5[file_index] , c6[file_index] , mass_point[file_index], eff, eff_err, dir_name , dir_flag , signal_background_flag , background_file_index ); 
+
+      }// end if
+
+
+
+
+  if(signal_background_flag == 1 )// read background sample
+      { 
+
+	if( file_index!= 0 )continue; // read background only once
+
+	    // file_index: signal mass points
+ 	    // background_file_index: which background sample 
+	    // dir_name: name of directory to save ROOT file 
+	    // dir_flag: HEEP id 
+
+        for (int background_file_index=0; background_file_index<4;background_file_index++){// which background sample you want to read
+
+
+  		cout<< "dir_name: " << dir_name <<endl;
+  		cout<< "file_index: "<< file_index << endl;
+  		cout<< "file_name[file_index]: "<< file_name[file_index] << endl;
+  		cout<< "mass_point[file_index]: "<< mass_point[file_index]<<endl;
+  		cout<< "dir_flag: "<< dir_flag <<endl;
+  		cout<< "background_file_index: "<< background_file_index <<endl;
+  		cout<< "background_file_name[background_file_index]: "<< background_file_name[background_file_index] <<endl;
+
+	xAna_ele_selection( background_file_name[background_file_index] , c1[file_index] , c2[file_index] , c3[file_index] , c4[file_index] , c5[file_index] , c6[file_index] , mass_point[file_index], eff, eff_err, dir_name , dir_flag , signal_background_flag , background_file_index );
+
+            }// end background_file_index loop
+      }// end if
+
+
+
+
+
+//  TString png1_name = Form("Zprime_Cut_Flow_M_%d.png",mass_point[i]);
+//  png1_name = dir_name + png1_name;
+//  c1[i]->cd();  c1[i]->SaveAs(png1_name);
+
+  }// loop file_index
+
+}// signal_background_flag
+
+}// end if
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
